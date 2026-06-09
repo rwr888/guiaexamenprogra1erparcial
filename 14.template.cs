e243012a8402dd2282b383f07c7cbb19f96483a7
rwr8855 <facturasrafa11@gmail.com>
2026-06-09 01:26:27 -0600

    feat: cod 14 comentado

--- 14.template.cs
+++ 14.template.cs
@@ -1,16 +1,22 @@
 /*Código de estructura base de un programa formal de C#. El
 código a editar debe ir incluido en la función Main.*/
 
+// Le dice al programa que use las herramientas del sistema (como escribir o leer en consola)
 using System;
 
+// Es el nombre del proyecto. Sirve para agrupar y organizar tu código
 namespace Plantilla{
-	
-	
-	class Program{
-		static void Main(string[] args){
-			
+    
+    // Es el contenedor principal (la clase) donde va a vivir nuestro programa
+    class Program{
+        
+        // Es el método principal. TODO lo que quieras que haga tu programa al arrancar 
+        // debe ir escrito aquí adentro, entre sus llaves { }.
+        static void Main(string[] args){
+            
+            // <-- Tu código a editar va aquí adentro 
            
-		}
-	}
-	
-}
+        } // Fin del Main
+    } // Fin de la clase Program
+    
+} // Fin del namespace
